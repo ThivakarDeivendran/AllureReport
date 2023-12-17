2ca4df6e5e4ef95eb222e96d5ef419feb7e066c0
ThivakarDeivendran <dthivakar1@gmail.com>
2023-12-17 22:44:11 +0530

    commit to ovthidei branch

---
+++ src/test/java/allureReportPackage/TestFile.java
@@ -0,0 +1,13 @@
+package allureReportPackage;
+
+public class TestFile {
+	
+	
+	public static void main(String[] args) {
+		System.out.println("Testing 1");
+		for(int i=0;i<=5;i++) {
+			System.out.println("Testing 1");
+		}
+	}
+
+}
